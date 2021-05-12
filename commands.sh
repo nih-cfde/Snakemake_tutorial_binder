1da08fc082240536d0720af2f23a3f1c140a17f0
Saranya Canchi <36385305+s-canchi@users.noreply.github.com>
2021-05-11 22:01:55 -0700

    update snakemake commands to specify # of CPU core

--- commands.sh
+++ commands.sh
@@ -7,15 +7,15 @@ echo "PS1='\w $ '" >> .bashrc
 samtools --version
 
 # Running a rule
-snakemake -p map_reads
-snakemake -p download_data
+snakemake -p map_reads -j2
+snakemake -p download_data -j2
 
 
 # Next, let's run more rules sequentially:
-snakemake -p download_data
-snakemake -p download_genome
-snakemake -p uncompress_genome
-snakemake -p index_genome_bwa
-snakemake -p map_reads
+snakemake -p download_data -j2
+snakemake -p download_genome -j2
+snakemake -p uncompress_genome -j2
+snakemake -p index_genome_bwa -j2
+snakemake -p map_reads -j2
 
 # Day 2
