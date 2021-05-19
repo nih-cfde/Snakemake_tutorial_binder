a508913bfb8b98f8cda03ccb9bfcb99ef6bd1fae
Marisa Lim <marisalim@users.noreply.github.com>
2021-05-19 09:21:48 -0700

    add cmds to terminal binder

---
+++ commands.sh
@@ -0,0 +1,19 @@
+# Day 1
+
+# Shorten the command prompt
+echo "PS1='\w $ '" >> .bashrc
+
+# Check if environment setup was successful:
+samtools --version
+
+# Running a rule
+snakemake -p map_reads -j2
+snakemake -p download_data -j2
+
+
+# Next, let's run more rules sequentially:
+snakemake -p download_data -j2
+snakemake -p download_genome -j2
+snakemake -p uncompress_genome -j2
+snakemake -p index_genome_bwa -j2
+snakemake -p map_reads -j2
